d138bdcb5eaf93a1083739c8bd50d0a317ecc77b
JbLb <jerome@jblb.net>
2016-10-11 22:24:13 +0200

    clean up test code

--- lumos/simple_test.py
+++ lumos/simple_test.py
@@ -1,39 +1,41 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 
-from packet import E131Packet
+# from packet import E131Packet
 from source import DMXSource
 
-data = [255] * 500
+# import socket
+# import struct
+import time
+
+
+data = [255] * 16
 
 # p = E131Packet(universe=1, data=data)
 # print p.packet
 
-import socket
-import struct
 
 # 239.256.0.1:5568
-UDP_IP = "239.255.0.1"
+# UDP_IP = "239.255.0.1"
 # UDP_IP = "127.0.0.1"
-UDP_PORT = 5568
-multicast_group = ("239.255.0.1", 5568)
+# UDP_PORT = 5568
+# multicast_group = ("239.255.0.1", 5568)
 
-sock = socket.socket(socket.AF_INET, # Internet
-                      socket.SOCK_DGRAM) # UDP
+# sock = socket.socket(socket.AF_INET, # Internet
+#                      socket.SOCK_DGRAM) # UDP
 
 # Set the time-to-live for messages to 1 so they do not go past the
 # local network segment.
-ttl = struct.pack('b', 1)
+# ttl = struct.pack('b', 1)
 
 
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+# sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
 # Set a timeout so the socket does not block indefinitely when trying
 # to receive data.
-sock.settimeout(0.2)
+# sock.settimeout(0.2)
 
 # sent = sock.sendto(p.packet, (UDP_IP, UDP_PORT))
 src = DMXSource()
-import time
 data[2]= 0
 
 for i in range(255):
@@ -43,5 +45,6 @@ for i in range(255):
     time.sleep(0.1)
     print(".")
 
+print("***************************************")
 data = [0] * 500
 src.send_data(data)
